c5e9c9172c2cc341c499edf9474cdee83a884593
Raidlucky <swordslayer07@gmail.com>
2021-04-09 15:36:33 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+for (let battery = 0; battery <= 100; battery = battery + 1) {
+    if (battery <= 20) {
+        console.log(battery + "% low Power");
+    } else {
+        console.log(battery + "%");
+    }
+}
+
+if (battery = 100) {
+    console.log("Fully Charged");
+}
+
